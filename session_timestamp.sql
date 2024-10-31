96103943e1d6aec691142220bc098832a5979abb
nik-swami <nikhil.swami@sjsu.edu>
2024-10-30 23:19:33 -0700

    Delete session_timestamp.sql

--- session_timestamp.sql
+++
@@ -1,9 +0,0 @@
-with session_data as (
-    SELECT
-        sessionId,
-        ts
-    FROM {{ source('raw_data', 'session_timestamp') }}
-    WHERE sessionId IS NOT NULL
-)
-
-select * from session_data
